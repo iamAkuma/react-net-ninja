f8e689ffe1760d6905da7f6bd5065ca7b58e0f3f
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-04 16:48:40 +0545

    learning react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css'
 
 function App() {
+
+  const title = 'Welcome'
+  const likes = 50;
+  // const person = { name: 'aKuma', age: 20 }
+
   return (
     <div className="App">
       <div className="content">
         <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> */}
+        {/* Can't output object */}
       </div>
     </div>
   );
