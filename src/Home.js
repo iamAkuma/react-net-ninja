a934211891b5d4bc0745917a8c4d67f0aad9c30c
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-05 17:10:17 +0545

    Learning React

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Home = () => {
+    const [name, setName] = useState('mario')
+    const [age, setAge] = useState(25)
+
+    const handleClick = () => {
+        setName('luigi')
+        setAge(30)
+    }
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name, e.target)
+    // }
     return (
         <div>
             <h1>HomePage</h1>
+            <p>{name} is {age} years old.</p>
+            <button onClick={handleClick}>Click me</button>
+            {/* <button onClick={(e) =>
+                handleClickAgain('Saral', e)
+            }>Click Me Again</button> */}
         </div>
     )
 }
