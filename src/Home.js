85a8ca387d05c2dd5132cdf3af59afb2d77ae063
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-07 18:09:23 +0545

    Learning React

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
@@ -22,21 +22,23 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const [name, setName] = useState('mario')
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
+
+    useEffect(() => {
+        console.log('Use Effect ran')
+        console.log(name)
+    }, [name])
+
     return (
         <div className='home'>
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')}
-                title="Mario's Blogs" /> */}
-            {/* <h1>HomePage</h1>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click me</button> */}
-            {/* <button onClick={(e) =>
-                handleClickAgain('Saral', e)
-            }>Click Me Again</button> */}
+            <button onClick={() => setName('luigi')}>Change Name</button>
+            <p>{name}</p>
         </div>
     )
 }
