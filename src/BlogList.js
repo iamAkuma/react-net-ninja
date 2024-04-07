19a6a044f7872ba4eb29f9a20e701402889c74d4
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-07 17:55:06 +0545

    learning react

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,6 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
+
+
     // const blogs = props.blogs;
     // const title = props.title;
 
@@ -12,6 +14,7 @@ const BlogList = ({ blogs, title }) => {
                     <div className="blog-preview" key={blog.id}>
                         <h2>{blog.title}</h2>
                         <p>Written by {blog.author}</p>
+                        <button onClick={() => handleDelete(blog.id)}>Delete</button>
                     </div>
                 ))}
             </div>
--- src/Home.js
+++ src/Home.js
@@ -22,11 +22,15 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
     return (
-        <div>
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')}
-                title="Mario's Blogs" />
+        <div className='home'>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')}
+                title="Mario's Blogs" /> */}
             {/* <h1>HomePage</h1>
             <p>{name} is {age} years old.</p>
             <button onClick={handleClick}>Click me</button> */}
