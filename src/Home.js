a265c442ef61c5844ace695c1e19a86df636bd9d
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-08 14:53:03 +0545

    Learning React

--- src/Home.js
+++ src/Home.js
@@ -18,19 +18,33 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true)
-
+    const [error, setError] = useState(null)
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogss')
+                .then(res => {
+                    console.log(res)
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data.')
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setBlogs(data)
+                    setIsPending(false)
+                    setError(null)
+                })
+                .catch(err => {
+                    setIsPending(false)
+                    setError(err.message)
+                })
+        }, 1000)
     }, [])
 
     return (
         <div className='home'>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     )
