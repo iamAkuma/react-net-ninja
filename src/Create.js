c76f045cecf9fe0d8ace251c2742d6fd2c0a1f7d
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-09 14:39:00 +0545

    Reacter Router DOM

---
+++ src/Create.js
@@ -0,0 +1,12 @@
+
+const Create = () => {
+    return (
+        <div>
+            <div className="create">
+                <h2>Add a New Blog</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Create
