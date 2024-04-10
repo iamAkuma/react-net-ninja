f598e0585668719d3f861364ed0f06c64ea4c981
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-10 18:16:54 +0545

    Redirect user to the Homepage using useNavigate hook

--- src/Create.js
+++ src/Create.js
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import { useHistory } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
     const [isPending, setIsPending] = useState(false)
-    const history = useHistory();
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,7 +20,7 @@ const Create = () => {
         }).then(() => {
             console.log('New Blog Added!')
             setIsPending(false)
-            history.push('/')
+            navigate('/')
         })
 
     }
