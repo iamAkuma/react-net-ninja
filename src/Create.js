5cebae0b0c25cb14dabaa7c72e6ea92735b9c759
Saral Bajracharya <rmanbajra@gmail.com>
2024-04-10 18:00:28 +0545

    Controller Input in Forms

--- src/Create.js
+++ src/Create.js
@@ -2,23 +2,46 @@ import { useState } from "react"
 
 const Create = () => {
     const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('mario')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author }
+        console.log(blog)
+    }
     return (
         <div>
             <div className="create">
                 <h2>Add a New Blog</h2>
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="">
                         Blog title:
                     </label>
-                    <input type="text" value={title} required />
+                    <input type="text"
+                        value={title}
+                        required
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
                     <label htmlFor="">Blog body:</label>
-                    <textarea required></textarea>
+                    <textarea
+                        required
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                    >
+                    </textarea>
                     <label htmlFor="">Blog Author: </label>
-                    <select>
+                    <select
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    >
                         <option value="mario">mario</option>
                         <option value="yoshi">yoshi</option>
                     </select>
                     <button>Add Blog</button>
+                    <p>{title}</p>
+                    <p>{body}</p>
+                    <p>{author}</p>
                 </form>
             </div>
         </div>
